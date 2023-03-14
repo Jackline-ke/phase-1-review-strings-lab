aca4646c057d8bdbbb7f33ed5ff34269c9729bf8
Jackline-ke <antoninahodhiambo@gmail.com>
2023-03-14 04:41:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+
+let welcomeMessage = "Welcome to Flatbook, " + " " + currentUser + "!";
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + " " + "GRACE HOPPER" + "!";
+
+let shortGreeting = "Welcome, " + " " + "Grace" + "!"; 
+ 
